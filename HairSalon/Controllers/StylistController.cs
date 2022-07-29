e2102e0fe3d3279ae2d4751895572ca82a69b3a9
FrankTimmons <franktimmonspdx@gmail.com>
2022-07-29 15:39:37 -0700

    Finish client views, research viewbag and how to route to controllers to support functionality of not being able to add a client without a stylist.

--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -23,6 +23,8 @@ namespace HairSalon.Controllers
 
     public ActionResult Create()
     {
+      List<Stylist> stylists = _db.Stylists.ToList();
+      ViewBag.Stylists = stylists;
       return View();
     }
 
